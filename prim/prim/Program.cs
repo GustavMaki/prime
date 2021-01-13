1ef0fcf47275528e49189ab9fbdbdae02da7e8f9
GustavMaki <gustav.siljamaki@gmail.com>
2021-01-13 12:52:18 +0100

    v.1

---
+++ prim/prim/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace prim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many prime numbers would you like to see?");
+            int n = int.Parse(Console.ReadLine());
+            MyMethod(n);
+            Console.ReadLine();
+        }
+
+        static void MyMethod(int n)
+        {
+            int x= 0;
+            int i;
+            for (i=2; x<n; i++)
+            {
+                if (i == 2 || i == 3 || i == 5)
+                {
+                    Console.WriteLine(i);
+                    x++;
+                }
+                if (i % 2 != 0 && i % 3 !=0 && i % 5 != 0) 
+                {
+                    Console.WriteLine(i);
+                    x++;
+                }
+            }
+        }
+    }
+
+}
